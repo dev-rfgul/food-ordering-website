2e6f2608ab66293baf1425326eaa0f3e1883fa81
dev-rfgul <rfgul587@gmail.com>
2025-06-01 14:47:08 +0500

    finalized the project

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('orders/', views.orders, name='orders'),
     path('cart/', views.cart, name='cart'),
     path('add-to-cart/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
+    path('order/<int:order_id>/update-status/', views.update_order_status, name='update_order_status'),
     path('remove-from-cart/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('checkout/', views.checkout, name='checkout'),
     path('create-payment/', views.create_payment, name='create_payment'),
